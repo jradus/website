f4cea419a06572cd354e76b82a146d1dfbe06be0
Jake Radus <jake.radus@gmail.com>
2020-07-08 16:53:35 -0400

    Clarify index.html & flesh out framework for pallet.js

---
+++ javascript/pallet.js
@@ -0,0 +1,13 @@
+let pallets = {
+   first: [ '#444' ],
+   second: [ '#444' ]
+}
+
+palletChooser = () => {
+    let pallet = Math.floor( (Math.random() * 5) + 1 );
+    if ( pallet === 1 ) {
+
+    } else if ( pallet === 2 ) {
+        
+    }
+}
