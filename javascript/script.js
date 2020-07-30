4277420e0fc5a520ed2fdbb55e0fbf1e635ef901
Jake Radus <jake.radus@gmail.com>
2020-07-30 18:11:16 -0400

    remove jquery

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,16 @@
-let pallets = {
-   first: [ '#444' ],
-   second: [ '#444' ]
-}
+// let pallets = {
+//    first: [ '#444' ],
+//    second: [ '#444' ]
+// }
 
-palletChooser = () => {
-    let pallet = Math.floor( (Math.random() * 5) + 1 );
-    if ( pallet === 1 ) {
+// palletChooser = () => {
+//     let pallet = Math.floor( (Math.random() * 5) + 1 );
+//     if ( pallet === 1 ) {
 
-    } else if ( pallet === 2 ) {
+//     } else if ( pallet === 2 ) {
         
-    }
-}
+//     }
+// }
 
 // $(".title").click(function() {
 //     $(".poem").slideToggle();
